48e2319884f60f7ca8f949f062bc4ebc761f17f8
David Martinez <davidmgilo@gmail.com>
2016-10-13 21:36:40 +0200

    hello world jquery

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,4 @@
-var $  = require('jquery');
+var $ = require('jquery');
 
 //Millor d'aquesta manera.
 window.$ = window.jQuery = require('jquery');
@@ -8,5 +8,5 @@ var _  = require('underscore');
 window._ = require('underscore');
 
 $(document).ready(function(){
-    $("#msgid").html("This is Hello World by JQuery");
+    $("#msgid").html("hello world");
 });
